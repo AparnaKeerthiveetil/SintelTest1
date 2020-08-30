08ba25775b861cb3685136d9ee06f4358f35619f
AparnaKeerthiveetil <70466568+AparnaKeerthiveetil@users.noreply.github.com>
2020-08-30 16:56:38 +0800

    Add files via upload
    A.2 Changes committed

--- SingtelTest/src/Solution.java
+++ SingtelTest/src/Solution.java
@@ -3,9 +3,18 @@ public class Solution {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//A.1
 		Bird bird = new Bird();
 		bird.walk();
 		bird.fly();
 		bird.sing();
+		//A.2
+		Bird obj = new Chicken();
+		obj.fly();
+		obj.sing();
+		Bird objduck = new Duck();
+		objduck.fly();
+		objduck.sing();
+		
 	}
 }
